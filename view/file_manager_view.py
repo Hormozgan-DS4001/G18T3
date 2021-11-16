fadb1bc034d0a591881a7ea06d3781ae21e295e1
kamandkrg <kamandkargar@gmail.com>
2021-11-16 23:04:01 +0330

    add scrollbar

--- view/file_manager_view.py
+++ view/file_manager_view.py
@@ -1,5 +1,6 @@
 from configure import Frame, Button, LabelFrame, Label, Tk, Entry
 from tkinter import ttk
+import tkinter
 
 
 class FileManagerPanel(Tk):
@@ -13,14 +14,29 @@ class FileManagerPanel(Tk):
         self.callback_memory = callback_memory
         self.callback_delete = callback_delete
         self.callback_upload = callback_upload
+        frm_console = Frame(self)
+        frm_console.grid(row=0, column=0)
+        my_canvas = tkinter.Canvas(self)
+        my_canvas.pack(side="left", fill="both", expand=1)
+        my_scroll = ttk.Scrollbar(self, otient="vertical", command=my_canvas.yview)
+        my_scroll.pack(side="right", fill="y")
+        my_canvas.config(yscrollcommand=my_scroll.set)
+        my_canvas.bind("<configure>", lambda e: my_canvas.config(scrollregion=my_canvas.bbox("all")))
+        self.frm = Frame(my_canvas)
+        my_canvas.create_window((0, 0), window=self.frm, anchor="nw")
 
-        
+    def update_memory(self, text):
+        pass
 
+    def add_file(self):
+        pass
 
+    def change_memory(self):
+        pass
 
+    def next_page(self):
+        pass
 
-
-
-
-
-
+    def prev_page(self):
+        pass
+    
