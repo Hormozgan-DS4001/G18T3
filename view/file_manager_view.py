9cb15bb6326835a28358272667b56108aedf4aa1
kamandkrg <kamandkargar@gmail.com>
2021-11-16 18:15:24 +0330

    add callback delete and upload

--- view/file_manager_view.py
+++ view/file_manager_view.py
@@ -3,13 +3,18 @@ from tkinter import ttk
 
 
 class FileManagerPanel(Tk):
-    def __init__(self, callback_new, callback_old, callback_more, callback_less, callback_memory):
+    def __init__(self, callback_new, callback_old, callback_more, callback_less, callback_memory,
+                 callback_delete, callback_upload):
         super(FileManagerPanel, self).__init__()
         self.callback_new = callback_new
         self.callback_old = callback_old
         self.callback_more = callback_more
         self.callback_less = callback_less
         self.callback_memory = callback_memory
+        self.callback_delete = callback_delete
+        self.callback_upload = callback_upload
+
+
 
 
 
