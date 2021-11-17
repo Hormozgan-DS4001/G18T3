a1678c7a9a9cf1ef8bb87171a759e491877b3ac6
kamandkrg <kamandkargar@gmail.com>
2021-11-17 18:23:18 +0330

    add model and method to model

---
+++ model/model.py
@@ -0,0 +1,28 @@
+
+class File:
+    def __init__(self, name: str, address: str, size: int):
+        self.name = name
+        self.address = address
+        self.size = size
+
+
+class Core:
+    def __init__(self):
+        self.memory = 0
+
+    def add_file(self):
+        pass
+
+    def delete_file(self):
+        pass
+
+    def change_memory(self):
+        pass
+
+
+
+
+
+
+
+
