41b190e791cb032dc7cfdbcc43b9053a40de3935
kamandkrg <kamandkargar@gmail.com>
2021-11-21 09:05:28 +0330

    fix __init__ in model

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+from data_structure import BST
+
 
 class File:
     def __init__(self, name: str, address: str, size: int):
@@ -9,8 +11,9 @@ class File:
 class Core:
     def __init__(self):
         self.memory = 0
+        self.file_list = BST()
 
-    def add_file(self):
+    def add_file(self, name, address, size):
         pass
 
     def delete_file(self):
